d42f66faa95c50a112ff6d85c904211302e69cdb
Aldiklisman <115341897+Aldiklisman@users.noreply.github.com>
2024-12-15 22:00:35 +0700

    Vertex

---
+++ Vertex.java
@@ -0,0 +1,11 @@
+public class Vertex {
+    String kelurahan;
+    boolean visited = false;
+    int distance = Integer.MAX_VALUE;
+    Vertex previousVertex;
+    Vertex next;
+
+    public Vertex(String kelurahan) {
+        this.kelurahan = kelurahan;
+    }
+}
