79bdf11d2ad5a2893457cb97d31b988e9e8b46e2
Aldiklisman <115341897+Aldiklisman@users.noreply.github.com>
2024-12-18 22:25:11 +0700

    Graph Method

--- EdgeList.java
+++ EdgeList.java
@@ -1,27 +1,3 @@
 public class EdgeList {
-    Edge head;
-
-    public class Edge {
-        Vertex destination;
-        int jarak;
-        Edge next;
-
-        public Edge(Vertex destination, int jarak) {
-            this.destination = destination;
-            this.jarak = jarak;
-        }
-    }
-    public void addEdge(Vertex destination, int jarak) {
-        Edge newEdge = new Edge(destination, jarak);
-        if (head == null) {
-            head = newEdge;
-        } else {
-            Edge temp = head;
-            while (temp.next != null) {
-                temp = temp.next;
-            }
-            temp.next = newEdge;
-        }
-    }
-}
+    
 }
